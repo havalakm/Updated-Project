2121eb621530b0eddcf9b0ac05dd1a333f28a46c
dibyaranjan <dibyaranjan.sathua@gmail.com>
2020-09-05 17:33:57 +0530

    Added comments

--- src/crawler/crawler/spiders/englishbraids.py
+++ src/crawler/crawler/spiders/englishbraids.py
@@ -11,6 +11,7 @@ class EnglishbraidsSpider(scrapy.Spider):
         super(EnglishbraidsSpider, self).__init__(*args, **kwargs)
         self.start_url = "http://englishbraids.com/"
         self.start_parse = self.parse_products
+        # List of dict where each dict has key as product category and value as list of products
         self.products = []
 
     def start_requests(self):
